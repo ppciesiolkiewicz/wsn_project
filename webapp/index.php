f088dfa26727ce860d99e5f4ebd6c67531bfdb29
Piotr Ciesiolkiewicz <ppciesiolkiewicz@gmail.com>
2015-04-22 21:01:39 +0200

    index.php added

---
+++ webapp/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<!-- jquery -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+<!-- Highcharts -->
+<script src="http://code.highcharts.com/highcharts.js"></script>
+<script src="http://code.highcharts.com/modules/data.js"></script>
+<script src="http://code.highcharts.com/modules/exporting.js"></script>
+</head>
+<body>
+<?php echo "Php works";?>
+<h1>It works!</h1>
+</body>
+</html>
